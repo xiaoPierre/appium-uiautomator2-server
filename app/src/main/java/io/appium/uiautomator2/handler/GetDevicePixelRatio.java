db564923fa548d8d165b9aba02254e92343a05cb
Mykola Mokhnach <mokhnach@gmail.com>
2021-04-14 18:15:18 +0200

    chore: Simplify retrieval of system metrics (#420)

--- app/src/main/java/io/appium/uiautomator2/handler/GetDevicePixelRatio.java
+++ app/src/main/java/io/appium/uiautomator2/handler/GetDevicePixelRatio.java
@@ -1,16 +1,25 @@
-package io.appium.uiautomator2.handler;
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import android.app.Instrumentation;
-import android.content.Context;
-import android.util.DisplayMetrics;
-import android.view.WindowManager;
+package io.appium.uiautomator2.handler;
 
 import io.appium.uiautomator2.handler.request.SafeRequestHandler;
 import io.appium.uiautomator2.http.AppiumResponse;
 import io.appium.uiautomator2.http.IHttpRequest;
-import io.appium.uiautomator2.utils.Logger;
-
-import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import io.appium.uiautomator2.model.internal.Gestures;
 
 public class GetDevicePixelRatio extends SafeRequestHandler {
 
@@ -18,18 +27,8 @@ public class GetDevicePixelRatio extends SafeRequestHandler {
         super(mappedUri);
     }
 
-    private static float getDeviceScaleRatio(Instrumentation instrumentation) {
-        WindowManager windowManager = (WindowManager) instrumentation.getContext().getSystemService(Context.WINDOW_SERVICE);
-        DisplayMetrics metrics = new DisplayMetrics();
-        windowManager.getDefaultDisplay().getMetrics(metrics);
-        return metrics.density;
-    }
-
     @Override
     protected AppiumResponse safeHandle(IHttpRequest request) {
-        Logger.info("Get device pixel ratio");
-        Instrumentation instrumentation = getInstrumentation();
-        Float ratio = getDeviceScaleRatio(instrumentation);
-        return new AppiumResponse(getSessionId(request), ratio);
+        return new AppiumResponse(getSessionId(request), Gestures.getDisplayDensity());
     }
 }
--- app/src/main/java/io/appium/uiautomator2/handler/GetSystemBars.java
+++ app/src/main/java/io/appium/uiautomator2/handler/GetSystemBars.java
@@ -1,12 +1,27 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.appium.uiautomator2.handler;
 
-import android.app.Instrumentation;
+import android.content.res.Resources;
 
 import io.appium.uiautomator2.handler.request.SafeRequestHandler;
 import io.appium.uiautomator2.http.AppiumResponse;
 import io.appium.uiautomator2.http.IHttpRequest;
 import io.appium.uiautomator2.model.api.SystemBarsModel;
-import io.appium.uiautomator2.utils.Logger;
 
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
@@ -18,20 +33,12 @@ public class GetSystemBars extends SafeRequestHandler {
 
     @Override
     protected AppiumResponse safeHandle(IHttpRequest request) {
-        Logger.info("Get status bar height of the device");
-
-        Instrumentation instrumentation = getInstrumentation();
-
-        int height = getStatusBarHeight(instrumentation);
-        return new AppiumResponse(getSessionId(request), new SystemBarsModel(height));
+        return new AppiumResponse(getSessionId(request), new SystemBarsModel(getStatusBarHeight()));
     }
 
-    private int getStatusBarHeight(Instrumentation instrumentation) {
-        int result = 0;
-        int resourceId = instrumentation.getContext().getResources().getIdentifier("status_bar_height", "dimen", "android");
-        if (resourceId > 0) {
-            result = instrumentation.getContext().getResources().getDimensionPixelSize(resourceId);
-        }
-        return result;
+    private static int getStatusBarHeight() {
+        Resources resources = getInstrumentation().getContext().getResources();
+        int resourceId = resources.getIdentifier("status_bar_height", "dimen", "android");
+        return resourceId > 0 ? resources.getDimensionPixelSize(resourceId) : 0;
     }
 }
